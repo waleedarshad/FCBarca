ce10bf2ccc6bf8d9e75dab1769a9d3de12ba82e0
vctrysam <sam@samokoro.com>
2016-09-10 13:39:09 -0500

    Cool Environment

---
+++ app/components/index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import {
+
+  StyleSheet,
+  Image,
+  Text,
+  TouchableOpacity,
+  Navigator,
+  View
+} from 'react-native';
+
+import Home from './home'
+
+
+export default class Index extends Component {
+  constructor(props){
+    super(props)
+ 
+  }
+ 
+  renderScene(route, navigator) {
+    var {state,actions} = this.props;
+    var routeId = route.id;
+
+    if (routeId === 'home') {
+      return (
+        <Home
+        {...this.props} 
+        navigator={navigator} />
+        );
+    }  
+   }
+
+
+  render() {
+    return (
+      <View style={{flex:1}}>
+     <Navigator
+     style={{flex: 1}}
+     ref={'NAV'}
+     initialRoute={{id: 'home', name: 'home'}}
+     renderScene={this.renderScene.bind(this)}/>
+        </View>
+    )
+}
+}
---
+++ index.ios.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import FCBarca from './app/container/';
+
+AppRegistry.registerComponent('FCBarca', () => FCBarca);
